cb25fa0a6591ad1e8ec330a35c2d0524cec92f42
Evgeniia Ialymova <eugeniyayal@gmail.com>
2019-08-28 14:44:03 +0300

    final

---
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,57 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    WebDriver wd;
+
+
+    private ContactHelper contactHelper;
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+    private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public void init() {
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        groupHelper = new GroupHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        contactHelper = new ContactHelper(wd);
+        sessionHelper.login("admin", "secret");
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
+    }
+
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
+    }
+
+    public ContactHelper getContactHelper() {
+        return contactHelper;
+    }
+}
